ae860a1b8765e7a718fbcb4bdf41966838ad5a9b
Jacob Clark <jacob.clark@bbc.co.uk>
2019-06-05 22:15:11 +0100

    construct compoundd data type

--- src/Recify.hs
+++ src/Recify.hs
@@ -23,6 +23,7 @@ import Clients.Spotify.RecentlyPlayed
 import Types.SpotifyAuthorization
 import Types.RecentlyPlayed
 import Types.Artist
+import qualified Types.RecentlyPlayedWithArtist as RPWA
 
 authorizationScope = "user-read-recently-played, user-top-read"
 authorizationResponseType = "code"
@@ -61,11 +62,34 @@ recify = scotty port $ do
 
           artists <- case maybeMarshalledArtistsData of
             Right (marshalledArtistsData) -> do 
-              return $ Just marshalledArtistsData
+              return $ marshalledArtistsData
             Left (error) -> 
-              return $ Nothing
+              return $ []
 
-            recentlyPlayedHTMLResponse <- liftIO $ getRecentlyPlayedHTMLResponse marshalledRecentlyPlayed
+          let tracks = RPWA.RecentlyPlayedWithArtist {
+            RPWA.recentlyPlayed = RPWA.Tracks {
+              RPWA.tracks = fmap (\track -> RPWA.Track {
+                RPWA.name = name $ track,
+                RPWA.externalUrls = externalUrls $ track,
+                RPWA.explicit = explicit $ track,
+                RPWA.artists = fmap (\artist -> RPWA.Artist {
+                  RPWA.id = Types.RecentlyPlayed.id $ artist,
+                  RPWA.href = href $ artist,
+                  RPWA.artistName = artistName $ artist,
+                  RPWA.genres = concat $ fmap (\artist -> Types.Artist.genres $ artist) artists
+                }) (Types.RecentlyPlayed.artists $ track),
+                RPWA.playedAt = playedAt $ track
+              }) . Types.RecentlyPlayed.tracks . recentlyPlayed $ marshalledRecentlyPlayed
+            },
+            RPWA.next = "x"
+          } 
+
+          let x = ((RPWA.genres ((RPWA.artists $ ((RPWA.tracks . RPWA.recentlyPlayed $ tracks) !! 0)) !! 0)) !! 0)
+          let y = (RPWA.name ((RPWA.tracks . RPWA.recentlyPlayed $ tracks) !! 0))
+          liftIO $ putStrLn x
+          liftIO $ putStrLn y
+
+          recentlyPlayedHTMLResponse <- liftIO $ getRecentlyPlayedHTMLResponse marshalledRecentlyPlayed
 
           let nextRecentlyPlayedTracksHref = getNextRecentlyPlayedTracksHref marshalledRecentlyPlayed
 
---
+++ src/Types/RecentlyPlayedWithArtist.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Types.RecentlyPlayedWithArtist where
+
+import qualified Data.ByteString.Lazy as L
+import qualified Data.Vector as V
+import Data.Aeson
+import Data.Either
+
+data Artist = Artist {
+  id :: String
+  , href :: String
+  , artistName :: String
+  , genres :: [String]
+} deriving (Show)
+
+data Track = Track {
+  playedAt :: String
+  , externalUrls :: String
+  , name :: String
+  , artists :: [Artist]
+  , explicit :: Bool
+} deriving (Show)
+
+data Tracks = Tracks {
+  tracks :: [Track]
+} deriving (Show)
+
+data RecentlyPlayedWithArtist = RecentlyPlayedWithArtist {
+  recentlyPlayed :: Tracks
+  , next :: String
+} deriving (Show)
