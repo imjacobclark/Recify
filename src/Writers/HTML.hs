889c74914e3ec78fef73e6566aea4e6ead5f8163
chris-bacon <chrisbacon2009@hotmail.com>
2019-06-07 11:34:07 +0100

    point free and monadio constraint

--- src/Writers/HTML.hs
+++ src/Writers/HTML.hs
@@ -15,19 +15,19 @@ import Utils.String
 getRecentlyPlayedHTMLResponse :: RPWA.RecentlyPlayedWithArtist -> IO LT.Text
 getRecentlyPlayedHTMLResponse recentlyPlayed = do
   _ <- writeCsvToDisk recentlyPlayed
-  return (stringToLazyText . buildRecentlyPlayedHTMLList . RPWA.tracks . RPWA.recentlyPlayed $ recentlyPlayed)
+  return . stringToLazyText . buildRecentlyPlayedHTMLList . RPWA.tracks . RPWA.recentlyPlayed $ recentlyPlayed
 
 getNextRecentlyPlayedTracksHref :: RPWA.RecentlyPlayedWithArtist -> LT.Text
-getNextRecentlyPlayedTracksHref recentlyPlayed = stringToLazyText . RPWA.next $ recentlyPlayed
+getNextRecentlyPlayedTracksHref = stringToLazyText . RPWA.next
 
 getArtistsFromTrack :: RPWA.Track -> String
 getArtistsFromTrack track = concat . intersperse ", " $ fmap (\artist -> ((RPWA.artistName artist) ++ " - " ++ (intercalate ", " $ RPWA.genres artist))) (RPWA.artists track)
 
 buildRecentlyPlayedHTMLList :: [RPWA.Track] -> String
 buildRecentlyPlayedHTMLList [] = []
-buildRecentlyPlayedHTMLList (tracks) = concat . intersperse "<br>" $ fmap (\track -> "<li>" ++ (RPWA.name track) ++ " - " ++ (Writers.HTML.getArtistsFromTrack track) ++ "</li>") tracks
+buildRecentlyPlayedHTMLList tracks = concat . intersperse "<br>" $ fmap (\track -> "<li>" ++ (RPWA.name track) ++ " - " ++ (Writers.HTML.getArtistsFromTrack track) ++ "</li>") tracks
 
--- buildResponse :: LT.Text -> LT.Text -> m LT.Text
+buildResponse :: MonadIO m => LT.Text -> LT.Text -> m LT.Text
 buildResponse recentlyPlayedHTML nextHTML = do
         liftIO $ putStrLn "Parsing Dashboard HTML"
         dashboardHtml <- liftIO . DTIO.readFile $ "./static/dashboard.html"
