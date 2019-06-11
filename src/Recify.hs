4a9b04b5e4089fb7a649b4ebc2250032ca16922a
Jacob Clark <jacob.clark@bbc.co.uk>
2019-06-11 12:20:21 +0100

    Debugging

--- src/Recify.hs
+++ src/Recify.hs
@@ -64,24 +64,32 @@ recify = do
       setHeader "Location" $ LT.pack "/dashboard"
 
     get "/csv" $ do
-      file <- liftIO $ readFile "./recentlyPlayed.csv"
+      file <- liftIO $ readFile "./data/recentlyPlayed.csv"
       html $ mconcat ["<pre>", LT.pack file, "</pre>"]
 
     get "/dashboard" $ do
+      liftIO $ putStrLn "Accepting request for /dashboard..."
       accessTokenData <- getAccessTokenFromCookies
+      liftIO $ putStrLn ("Got access token " ++ (show accessTokenData))
       let cookie = DT.pack . LT.unpack . snd $ accessTokenData !! 0 -- need to check if anything exists in the array and die if not 
+      liftIO $ putStrLn ("Parsed access token " ++ (show cookie))
       let accessToken = textToByteString . getAccessToken . AccessToken $ cookie -- if this is empty we need to stop as authorization hasn't occoured 
-
+      liftIO $ putStrLn ("Marshalled access token " ++ (show accessToken))
+      
       recentlyPlayedTrackData <- liftIO . fetchRecentlyPlayedTracks $ accessToken
+      liftIO $ putStrLn ("Fetched recently played tracks" ++ (show recentlyPlayedTrackData))
       let maybeMarshalledRecentlyPlayed = marshallRecentlyPlayedData recentlyPlayedTrackData
+      liftIO $ putStrLn ("Marshalled recently played tracks" ++ (show maybeMarshalledRecentlyPlayed))
       
       case maybeMarshalledRecentlyPlayed of
         Right marshalledRecentlyPlayed -> do
           maybeMarshalledArtistsData <- liftIO . getArtistData accessToken $ marshalledRecentlyPlayed
+          liftIO $ putStrLn "Fetched artist data"
 
           artists <- case maybeMarshalledArtistsData of
             Right marshalledArtistsData -> return marshalledArtistsData
             Left error                  -> return [] -- TODO this needs to error, no point continuing
+          liftIO $ putStrLn ("Marshalled artist data" ++ (show artists))
 
           let recentlyPlayedWithArtist = RPWA.RecentlyPlayedWithArtist {
             RPWA.recentlyPlayed = RPWA.Tracks {
@@ -100,12 +108,15 @@ recify = do
             },
             RPWA.next = Types.RecentlyPlayed.next $ marshalledRecentlyPlayed
           }
+          liftIO $ putStrLn ("Marshalled recentlyPlayedWithArtist data " ++ (show recentlyPlayedWithArtist))
 
           recentlyPlayedHTMLResponse <- liftIO . getRecentlyPlayedHTMLResponse $ recentlyPlayedWithArtist
-
           let nextRecentlyPlayedTracksHref = getNextRecentlyPlayedTracksHref recentlyPlayedWithArtist
+          liftIO $ putStrLn ("Created HTML response" ++ (show nextRecentlyPlayedTracksHref))
 
           response <- buildResponse recentlyPlayedHTMLResponse nextRecentlyPlayedTracksHref
+          liftIO $ putStrLn ("Built HTML response" ++ (show response))
+
           html $ mconcat [response]
         Left error ->
           html $ mconcat ["Something went wrong getting data from Spotify, refresh to try again."]
